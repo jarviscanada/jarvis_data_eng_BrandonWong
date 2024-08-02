3245bfb4a7d30bd2cf334580d54bc3730f7aba47
Brandon Wong <wen.wong@mail.mcgill.ca>
2024-08-02 15:44:15 +0000

    feat: Add introduction to queries module

---
+++ sql/README.md
@@ -0,0 +1,5 @@
+# Introduction
+
+# SQL Queries
+## Table Setup (DDL)
+## Question 1: Insert a row
---
+++ sql/queries.sql
@@ -0,0 +1,5 @@
+-- Introduction
+-- All SQL module queries will be placed under this file as reference
+
+-- Question 1: Insert a row
+
