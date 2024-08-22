19249c5d47db05e234779f302e5457d836de5111
wen-wong <wen.wong@mail.mcgill.ca>
2024-08-22 12:27:06 -0400

    feat: Add regex exercise testing layer

--- core_java/grep/src/main/java/ca/jrvs/apps/practice/RegexExc.java
+++ core_java/grep/src/main/java/ca/jrvs/apps/practice/RegexExc.java
@@ -1,9 +1,29 @@
 package ca.jrvs.apps.practice;
 
 public interface RegexExc {
-    public boolean matchJpeg(String filename);
+    /**
+     * Match given filename with a jpeg format.
+     * Ensure that filename ends with `.jpg` and `.jpeg`
+     *
+     * @param filename
+     * @return a Jpeg match has been found
+     */
+    boolean matchJpeg(String filename);
 
-    public boolean matchIp(String ip);
+    /**
+     * Match given ip with a ip format.
+     * Ensure that the ip is between 0.0.0.0 to 999.999.999.999
+     *
+     * @param ip
+     * @return a valid ip has been given
+     */
+    boolean matchIp(String ip);
 
-    public boolean isEmptyLine(String line);
+    /**
+     * Match a given line with an empty line
+     *
+     * @parama line
+     * @return an emptu line has been given
+     */
+    boolean isEmptyLine(String line);
 }
